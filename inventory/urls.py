09c1d36b6a744e80ff741c4f2376ced19b1ea8c1
Carl Vincel Gale <carlgale72@gmail.com>
2026-09-20 06:35:47 +0800

    Fix the routes of inventory

--- inventory/urls.py
+++ inventory/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("/products", views.ProductsViewList.as_view(), name="all-products"),
-    path("/products/api", views.view_products_api, name="all-products-api"),
-    path("/product/<slug:slug>", views.view_product, name="view-product")
+    path("products", views.ProductsViewList.as_view(), name="all-products"),
+    path("products/api", views.view_products_api, name="all-products-api"),
+    path("product/<slug:slug>", views.view_product, name="view-product")
 ]
